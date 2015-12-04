b6c09c899d4c53a4e0253b793452745d2bd1d7eb
Jonathan Zhang <jonathan.z99@gmail.com>
2015-12-03 16:38:37 -0800

    more lower cases

--- config/passport.js
+++ config/passport.js
@@ -43,7 +43,7 @@ module.exports = function(passport) {
 
 		// find a user whose email is the same as the forms email
 		// we are checking to see if the user trying to login already exists
-        User.findOne({ 'local.email' :  email }, function(err, user) {
+        User.findOne({ 'local.email' :  email.toLowerCase() }, function(err, user) {
             // if there are any errors, return the error
             if (err)
                 return done(err);
@@ -58,7 +58,7 @@ module.exports = function(passport) {
                 var newUser            = new User();
 
                 // set the user's local credentials
-                newUser.local.email    = email;
+                newUser.local.email    = email.toLowerCase();
                 newUser.local.password = newUser.generateHash(password); // use the generateHash function in our user model
                 console.log("got post");
             		console.log(req.body.firstname);
