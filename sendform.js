175885360141ca402face2359a932c31a92ac0d3
Kevin Frans <kevin@bobthechicken.com>
2016-01-22 10:01:49 -0800

    forms

--- sendform.js
+++ sendform.js
@@ -6,6 +6,17 @@ var regis = db.users.find({"local.statesRegistered": 1});//var email = ["jonatha
 while (regis.hasNext()) {
     current =  regis.next();
     print(current.local.email);
+
+    sendgrid.send({
+		  to:       current.local.email,
+		  from:     'kevin@gunndeca.org',
+		  subject:  'States Forms',
+		  text:
+		  'Thanks for signing up for states! Forms: ',
+		}, function(err, json) {
+		  if (err) { return console.error(err); }
+		  console.log(json);
+		});
 }
 
 // for (var i = 0; i < email.length; i++){
